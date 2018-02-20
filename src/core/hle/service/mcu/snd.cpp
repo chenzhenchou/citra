19d7196c4bdd0148a2c573e9b26e4bcbaceec7b0
Valentin Vanelslande <valentinvanelslande0@gmail.com>
2018-02-20 08:59:40 -0600

    mcu::SND: Remove unknown functions

--- src/core/hle/service/mcu/snd.cpp
+++ src/core/hle/service/mcu/snd.cpp
@@ -10,7 +10,6 @@ namespace MCU {
 SND::SND(std::shared_ptr<Module> mcu) : Module::Interface(std::move(mcu), "mcu::SND", 1) {
     static const FunctionInfo functions[] = {
         {0x00010080, nullptr, "GetSoundVolume"},
-        {0x00020040, nullptr, "unk_0x00020040"},
         {0x00030000, nullptr, "GetRegister25h"},
     };
     RegisterHandlers(functions);
