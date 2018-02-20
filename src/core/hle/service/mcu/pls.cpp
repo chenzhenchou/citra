baed0f32c8b5641cfffd3422812637cf7647e16f
Valentin Vanelslande <valentinvanelslande0@gmail.com>
2018-02-20 17:02:18 -0600

    [skip ci] mcu::PLS: fix clang

--- src/core/hle/service/mcu/pls.cpp
+++ src/core/hle/service/mcu/pls.cpp
@@ -9,15 +9,10 @@ namespace MCU {
 
 PLS::PLS(std::shared_ptr<Module> mcu) : Module::Interface(std::move(mcu), "mcu::PLS", 1) {
     static const FunctionInfo functions[] = {
-        {0x00010000, nullptr, "GetDatetime"},
-        {0x00020000, nullptr, "GetSeconds"},
-        {0x00030000, nullptr, "GetMinutes"},
-        {0x00040000, nullptr, "GetHour"},
-        {0x00050000, nullptr, "GetDayOfWeek"},
-        {0x00060000, nullptr, "GetDay"},
-        {0x00070000, nullptr, "GetMonth"},
-        {0x00080000, nullptr, "GetYear"},
-        {0x00090000, nullptr, "GetTickCounter"},
+        {0x00010000, nullptr, "GetDatetime"},    {0x00020000, nullptr, "GetSeconds"},
+        {0x00030000, nullptr, "GetMinutes"},     {0x00040000, nullptr, "GetHour"},
+        {0x00050000, nullptr, "GetDayOfWeek"},   {0x00060000, nullptr, "GetDay"},
+        {0x00070000, nullptr, "GetMonth"},       {0x00080000, nullptr, "GetYear"},
     };
     RegisterHandlers(functions);
 }
