fece9f5dbdd248917da9b22cf7088aa74954d14d
Valentin Vanelslande <valentinvanelslande0@gmail.com>
2018-03-18 20:24:41 -0600

    fix?

--- src/core/hle/service/am/am.cpp
+++ src/core/hle/service/am/am.cpp
@@ -726,7 +726,7 @@ void Module::Interface::GetProductCode(Kernel::HLERequestContext& ctx) {
 
     FileUtil::IOFile f(path, "rb");
     f.Seek(0x150, 0);
-    f.ReadBytes(&ctx.CommandBuffer()[2], 0x10);
+    f.ReadBytes(&Kernel::GetCommandBuffer()[2], 0x10);
     f.Close();
     rb.Push(RESULT_SUCCESS);
 }
